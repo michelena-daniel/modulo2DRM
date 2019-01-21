415114e77173fcb4d68c2ab6ca02387055073a05
michelena-daniel <danielromero.sem@gmail.com>
2019-01-21 21:37:08 +0100

    dia1

---
+++ main.js
@@ -0,0 +1,31 @@
+// let nombre;
+// let edad = 24;
+// const cumple = "6/6/666";
+// nombre = "Daniel";
+// alert("Mi nombre es " + nombre + " y tengo " + edad + " años.");
+// console.log("Mi nombre es " + nombre);
+// console.log(`Mi edad es ${edad}`);
+// const COLOR_ORANGE = "#FF700";
+
+// working with variables
+//ejericio 1
+// 1
+
+let admin;
+let name;
+
+// 2
+name = "John";
+
+//3
+admin = name;
+
+//4
+alert(admin);
+
+//ejercicio 2
+
+let planet = "Earth";
+let visitor;
+
+//ejercicio 3
