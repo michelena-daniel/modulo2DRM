1738236890d628214a8d495ba05095a3871428ce
michelena-daniel <danielromero.sem@gmail.com>
2019-01-26 11:11:58 +0100

    dia4fin

--- dia 4/pruebas/app.js
+++ dia 4/pruebas/app.js
@@ -1,100 +1,126 @@
-/*
- * Aquí podéis hacer los ejercicios y
- * practicar Javascript!
- */
-
-console.log("Probando, probando, 1, 2, 3!");
-
-// for (let i = 0; i < 100; i = +prompt("Ingrese un numero: "));
-
-// let num = prompt("Insert a number");
-
-// nextInt: for (let i = 2; i <= num; ++i) {
-//   for (let j = 2; j < i; j++) {
-//     if (i % j == 0) continue nextInt;
-//   }
-
-//   alert(i);
-// }
-
-// switch (browser) {
-//     case "Edge":
-//       alert("You've got the Edge!");
-//       break;
-
-//     case "Chrome":
-//     case "Firefox":
-//     case "Safari":
-//     case "Opera":
-//       alert("Okay we support these browsers too");
-//       break;
-
-//     default:
-//       alert("We hope that this page looks ok!");
-//   }
-
-//   if(browser = "Edge"){
-//       alert("You've got the edge!")
-//   }else if(browser = "Chrome" || "Firefox" || "Safari" || "Opera"){
-//     alert("Okay we support those too")
-//   }else{
-//       alert("We hope that this page looks ok!")
-//   }
-
-/*
-
-let a = +prompt("a?", "");
-
-switch (a) {
-  case 0:
-    alert(0);
-    break;
-  case 1:
-    alert(1);
-    break;
-  case 2:
-  case 3:
-    alert("2, 3");
-    break;
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
 }
 
-*/
-
-let age = 15;
-
-if (age >= 14 && age <= 90) {
-  alert("El bicho");
-}
-
-//lo contrario
-
-let age = 14;
-
-if (!(age >= 14 && age <= 90)) {
-  alert("El bicho.");
-}
-
-// cual de estos alerts se va a ejecuta
-
-let input = prompt("Insert input: ");
-
-function isCanceled(str) {
-  return str == "" || str == null;
-}
-
-if (input == "admin") {
-  let pass = prompt("Insert password: ");
-  if (pass == "The Master") {
-    alert("Welcome");
-  } else if (isCanceled) {
-    alert("canceled");
-  } else {
-    alert("Wrong password.");
+ask(
+  "Do you agree?",
+  function() {
+    alert("You agreed.");
+  },
+  function() {
+    alert("You canceled the execution.");
   }
-} else if (isCanceled) {
-  alert("canceled");
-} else {
-  alert("No te conozco");
+);
+
+// to arrow functions
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
 }
 
-// crear una función que convierta un número entero en un número roman
+ask(
+  "Do you agree?",
+
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
+
+//excecise HELLO OBJECT
+
+let user = new Object();
+
+user = {
+  name: "John",
+  surname: "Smith"
+};
+
+user.name = "Pete";
+
+delete user.surname;
+
+alert(user.name + " " + user.surname);
+
+//excercise isEmpty
+
+function isEmpty(schedule) {
+  for (let key in schedule) {
+      return false;
+    }
+    return true;
+  }
+  
+let schedule = {};
+
+alert(isEmpty(schedule)); // true
+
+schedule["8:30"] = "get up";
+
+alert(isEmpty(schedule)); // false
+
+// excercise const object
+
+const user = {
+  name: "John"
+};
+
+*!*
+// does it work?
+user.name = "Pete";
+*/!*
+
+// Sí, es posible
+
+//excercise SUM OBJECT PROPERTIES
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+};
+
+let sum = 0, i;
+
+for(i in salaries){
+  sum += salaries[i]
+}
+
+// Multiply numeric propertie by 2
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+function multiplyNumeric(objmenu){
+  for(let key in menu){
+    if(typeof menu[key] == "number")
+    menu[key] *=2;
+  }
+}
+
+multiplyNumeric(menu);
+
+alert(menu.title);
+
+
+// objeto que tenga una propiedad o dos y la tercera sea name. Crear una función que modifique esa propiedad name y la capitalize.
+
+let object = {
+  casa: "grande",
+  perro: "diero",
+  name: "dani"
+}
+
+function capitalize(object){
+  for(let key in obj){
+
+  object[key] = toUpperCase();
+  
+}
+}
+alert(object.name);
+
